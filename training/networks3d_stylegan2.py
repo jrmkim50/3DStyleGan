aa8fdecc12cd07f6c7ce310340d58e507ad24fab
jrmkim50 <jrmkim50@gmail.com>
2022-01-10 11:13:14 -0500

    added a pdb trace

--- training/networks3d_stylegan2.py
+++ training/networks3d_stylegan2.py
@@ -155,7 +155,7 @@ def minibatch_stddev_layer(x, group_size=4, num_new_features=1):
 # Main generator network.
 # Composed of two sub-networks (mapping and synthesis) that are defined below.
 # Used in configs B-F (Table 1).
-
+import pdb
 def G_main(
     latents_in,                                         # First input: Latent vectors (Z) [minibatch, latent_size].
     labels_in,                                          # Second input: Conditioning labels [minibatch, label_size].
@@ -175,6 +175,7 @@ def G_main(
     synthesis_func          = 'G_synthesis_stylegan2_3d_curated_real',  # Build func name for the synthesis network.
     **kwargs):                                          # Arguments for sub-networks (mapping and synthesis).
 
+    pdb.set_trace()
     # Validate arguments.
     assert not is_training or not is_validation
     assert isinstance(components, dnnlib.EasyDict)
