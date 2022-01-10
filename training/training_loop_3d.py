eb6be09e52920b2624818a998713a90ec7e21795
jrmkim50 <jrmkim50@gmail.com>
2022-01-10 10:49:25 -0500

    some debugging

--- training/training_loop_3d.py
+++ training/training_loop_3d.py
@@ -184,7 +184,7 @@ def training_loop(
     G.print_layers(); D.print_layers()
     sched = training_schedule(cur_nimg=total_kimg*1000, training_set=training_set, **sched_args)
     grid_latents = np.random.randn(np.prod(grid_size), *G.input_shape[1:])
-
+    pdb.set_trace()
     grid_fakes = Gs.run(grid_latents, grid_labels, is_validation=True, minibatch_size=sched.minibatch_gpu) 
 
     misc.save_3d_image_grid(grid_fakes, dnnlib.make_run_dir_path('fakes_init.png'), drange=drange_net, grid_size=grid_size)
@@ -366,7 +366,6 @@ def training_loop(
                 print( "=========================================" )
                 print( "Fast Path run G_train_op" )
                 print( "=========================================" )
-                pdb.set_trace()
                 v = tflib.run([G_train_op, data_fetch_op], feed_dict)
                 
                 if run_G_reg:
