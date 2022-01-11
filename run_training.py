4c51eeab74ae33137df190c87aaf5fb68e5e1ebc
jrmkim50 <jrmkim50@gmail.com>
2022-01-11 11:03:13 -0500

    updated minibatch size and fmap_min/fmap_max

--- run_training.py
+++ run_training.py
@@ -56,15 +56,15 @@ def run(dataset, data_dir, result_dir, config_id, num_gpus, total_kimg, gamma, m
 
         # Synthesis Network Params
         # G.resolution = 128
-        G.fmap_min = 64
-        G.fmap_max = 64
+        G.fmap_min = 16
+        G.fmap_max = 16
         G.base_size = [ 2, 2, 5 ]
 
         # Discriminator Params 
         D.architecture = 'resnet'        
         # D.resolution=128
-        D.fmap_min = 64
-        D.fmap_max = 64
+        D.fmap_min = 16
+        D.fmap_max = 16
         D.base_size = [ 2, 2, 5 ]
 
         train.data_dir = data_dir
@@ -75,7 +75,7 @@ def run(dataset, data_dir, result_dir, config_id, num_gpus, total_kimg, gamma, m
 
         sched.G_lrate_base = sched.D_lrate_base = 0.002
 
-        sched.minibatch_gpu_base = 8
+        sched.minibatch_gpu_base = 4
         sched.minibatch_gpu_dict = {}
         # sched.minibatch_gpu_dict = {8: 8, 16: 8, 32: 8, 64: 2, 128: 2}
 
