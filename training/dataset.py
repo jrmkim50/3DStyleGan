db856e2e13fe809c8560292fdccad0502fd198bd
jrmkim50 <jrmkim50@gmail.com>
2022-03-05 16:31:06 -0800

    updated shuffle_mb size

--- training/dataset.py
+++ training/dataset.py
@@ -29,7 +29,7 @@ class TFRecordDataset:
         max_label_size  = 0,        # 0 = no labels, 'full' = full labels, <int> = N first label components.
         max_images      = None,     # Maximum number of images to use, None = use all images.
         repeat          = True,     # Repeat dataset indefinitely?
-        shuffle_mb      = 2048,     # Shuffle data within specified window (megabytes), 0 = disable shuffling.
+        shuffle_mb      = 1024,     # Shuffle data within specified window (megabytes), 0 = disable shuffling.
         prefetch_mb     = 1024,     # Amount of data to prefetch (megabytes), 0 = disable prefetching.
         buffer_mb       = 256,      # Read buffer size (megabytes).
         num_threads     = 2,        # Number of concurrent threads.
